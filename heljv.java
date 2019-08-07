8a1cd82ebdc51b990fa26a3c8a70e87a13d5ab2d
Ubuntu <ubuntu@ip-172-31-11-180.ec2.internal>
2019-08-07 10:39:56 +0000

    I am copy all files to Github

---
+++ heljv.java
@@ -0,0 +1,11 @@
+class HelloJava {
+
+public static void main(String args[]){
+
+System.out.Println(“My first Java Program. Thank you! ”);
+
+}
+
+}
+
+
---
+++ ind.js
@@ -0,0 +1,5 @@
+var texts="My Name Partha";
+
+console.log(texts);
+
+
